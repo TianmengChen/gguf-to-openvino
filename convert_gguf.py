ea40ccd3bf64f00f83bf0d4103f83162f5f98999
TianmengChen <tianmeng.chen@intel.com>
2025-03-17 14:17:05 +0800

    q6k to int8 work, confirmed with qwen2.5 7B q40, but need to go deeper

--- convert_gguf.py
+++ convert_gguf.py
@@ -376,8 +376,34 @@ def make_fp16_weights(key, consts, reorder, head_size):
     w_f32 = opset.convert(weights, Type.f32)
     return w_f32
 
-# def make_q6k_int8_weights(key, consts, reorder, head_size):
+def make_q6k_int8_weights(key, consts, reorder, head_size):
+    weight = consts[f"{key}.weight"]
+    # weight = weight.view(np.uint8)
+    orig_weight_shape = list(weight.shape)
+    weight = weight.reshape(orig_weight_shape[0], -1, 16)
+    scale = np.expand_dims(consts[f"{key}.scales"], axis=2)
+    bias = np.expand_dims(consts[f"{key}.biases"], axis=2)
+    if reorder:
+        weight = reorder_interleaved_format(weight, head_size)
+        scale = reorder_interleaved_format(scale, head_size)
+        bias = reorder_interleaved_format(bias, head_size)
 
+    weights = opset.constant(weight, dtype=np.int8)
+    weights.set_friendly_name(name=f"{key}.weight")
+    weights_f16 = opset.convert(weights, Type.f16)
+
+    zero_point = (-bias / scale).astype(np.int8)
+    zero_points = opset.constant(zero_point, dtype=np.int8)
+    zero_points_f16 = opset.convert(zero_points, Type.f16)
+
+    scales = opset.constant(scale, dtype=np.float16)
+
+    w_zp = opset.subtract(weights_f16, zero_points_f16, auto_broadcast="numpy")
+    w_zp_s = opset.multiply(w_zp, scales, auto_broadcast="numpy")
+
+    w_zp_s_r = opset.reshape(w_zp_s, opset.constant(orig_weight_shape, dtype=np.int64), special_zero=False)
+    w_zp_s_f32 = opset.convert(w_zp_s_r, Type.f32)
+    return w_zp_s_f32
 
 
 def make_int8_weights(key, consts, reorder, head_size):#weight = ov.Tensor(weight, weight.shape, const_dtype)
@@ -457,8 +483,7 @@ def make_weights_subgraph(key, consts, qtype, reorder, head_size):
     elif "Q4_0" in qtype:
         final_node = make_int4_weights(key, consts, reorder, head_size)
     elif "Q6_K" in qtype:
-        # final_node = make_q6k_int8_weights(key, consts, reorder, head_size)
-        final_node = make_fp16_weights(key, consts, reorder, head_size)
+        final_node = make_q6k_int8_weights(key, consts, reorder, head_size)
     else:
         raise ValueError("Unsupported quantization type:")
     
