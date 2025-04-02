be055d919e8a152e68df5ef165ace5f23c9dea31
TianmengChen <tianmeng.chen@intel.com>
2025-04-02 17:20:05 +0800

    add test script for converted IR

---
+++ test.py
@@ -0,0 +1,22 @@
+
+import openvino_genai
+from transformers import set_seed
+
+def main():
+    set_seed(42)
+    model_dir = 'qwen'
+
+    device = 'CPU'  # GPU can be used as well
+    pipe = openvino_genai.LLMPipeline(model_dir, device)
+
+    config = openvino_genai.GenerationConfig()
+    config.max_new_tokens = 100
+    prompt="请解释一下欧拉角"
+    pipe.start_chat()
+    result = pipe.generate(prompt, config)
+    print(result)
+    pipe.finish_chat()
+
+
+if '__main__' == __name__:
+    main()
